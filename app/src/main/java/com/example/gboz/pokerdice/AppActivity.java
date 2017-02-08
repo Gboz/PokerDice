6a56fd1dfe934af51aef6ccd0df31804e9b23642
Gboz <gbolesta21@gmail.com>
2017-02-08 15:38:38 +0100

    – Create new activity: EngineActivity; – Add in this activity xml file: radio group and two radio buttons to change app language.

--- app/src/main/java/com/example/gboz/pokerdice/AppActivity.java
+++ app/src/main/java/com/example/gboz/pokerdice/AppActivity.java
@@ -1,14 +1,34 @@
 package com.example.gboz.pokerdice;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AppActivity extends AppCompatActivity {
+
+    Button newGameButton, engineButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app);
+
+        newGameButton = (Button) findViewById(R.id.newGameButton);
+        engineButton = (Button) findViewById(R.id.engineButton);
+
+        engineButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadEngineActivity(v);
+            }
+        });
+    }
+
+    public void loadEngineActivity(View view) {
+        Intent intent = new Intent(this, EngineActivity.class);
+        startActivity(intent);
     }
 
 }
