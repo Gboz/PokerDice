47b64ac29acb4878edf144e99a057b839f73ad7a
Gboz <gbolesta21@gmail.com>
2017-02-09 18:59:39 +0100

    – prepare checklist of players: add example elements

--- app/src/main/java/com/example/gboz/pokerdice/NewGameActivity.java
+++ app/src/main/java/com/example/gboz/pokerdice/NewGameActivity.java
@@ -4,20 +4,27 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ListView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class NewGameActivity extends AppCompatActivity {
 
     Button playButton, newPlayerButton;
+    String value;
+    String[] superStarNames = {"Mietek", "Agaciuga", "Pepi B", "Roman Boskyyy"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_game);
 
-        //disable the screensaver
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        // initiate a ListView
+        ListView listView = (ListView) findViewById(R.id.listView);
+        // set the adapter to fill the data in ListView
+        CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), superStarNames);
+        listView.setAdapter(customAdapter);
 
         playButton = (Button) findViewById(R.id.play_button);
         playButton.setOnClickListener(new View.OnClickListener() {
@@ -36,6 +43,28 @@ public class NewGameActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     public void loadPlayActivity(View view) {
         Intent intent = new Intent(this, PlayActivity.class);
         startActivity(intent);
@@ -45,4 +74,6 @@ public class NewGameActivity extends AppCompatActivity {
         Intent intent = new Intent(this, NewPlayerActivity.class);
         startActivity(intent);
     }
+
+
 }
