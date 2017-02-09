23a652a13393762dba8acb0fa34f397dfedefe4c
Gboz <gbolesta21@gmail.com>
2017-02-09 11:18:45 +0100

    – simple example SQLite database: for players table

--- app/src/main/java/com/example/gboz/pokerdice/NewPlayerActivity.java
+++ app/src/main/java/com/example/gboz/pokerdice/NewPlayerActivity.java
@@ -2,12 +2,17 @@ package com.example.gboz.pokerdice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class NewPlayerActivity extends AppCompatActivity {
+
+    Button createButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_player);
+
     }
+
 }
